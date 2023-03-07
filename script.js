5332a421d4b879fb3118c5385b5a18b047269ae4
tiko malania <124159302+malana1@users.noreply.github.com>
2023-03-07 23:07:34 +0400

    Add files via upload
    hw5

---
+++ script.js
@@ -0,0 +1,106 @@
+ "use strict";
+//1
+//let array = [12, 25, 3, 6, 8, 14, 7, 23];
+
+//let newarray =array.map(function(x) {
+ 
+//return x/3;
+//})
+  // console.log(newarray); 
+
+   //2
+//let languages = ['html', 'css', 'javascript',  'php'];
+
+//let newarray = languages.filter(item => item.length >3); 
+   
+//console.log(newarray);
+
+//3
+//let words = ['Madrid', 'Rome', 'Milan', 'Berlin'];
+ //let newwords = words.filter(x => x.length = "m" , "M"); 
+ //console.log(newwords);
+ // berlinic damibruna da ratom vre mivxvdi:(
+
+ //4
+//let arr1 = [1, 2];
+//let arr2 = [3, 4];
+//let arr3 = [5, 6];
+//let array = arr1. concat(arr2,arr3);
+//console.log(array);
+
+//5
+//let arr = [-1, -2, -3, 4].some(item => item <0);
+//console.log(arr);
+
+//6
+//let array =[23,45,32,5,87,7,3,98];  
+//array.sort(function(x,y) {
+ // return y-x; 
+//})
+//console.log(array);
+
+//9
+//let array=[2,15,10,24]
+//array.splice(2,1);
+//console.log(array);
+
+//7
+//let newptag = document.createElement('p');
+//.log(newptag);
+//newptag.textContent = 'hello';
+//newptag.setAttribute("class", "description");
+//document.getElementById('wraper').
+//appendChild(newptag);
+//newptag.style.color = "red";
+//newptag.style.fontSize = "30px";
+
+
+
+
+
+
+    
+
+
+    
+ 
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+ 
+
+ 
+    
+ 
+
+ 
+
+ 
+
+
+  
+  
+   
+   
+
+
+ 
+
+
+
+
+
+
+
+ 
